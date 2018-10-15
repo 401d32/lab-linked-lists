c3b50504c68b7bd0b25679eecca30faa87bfd7f0
cara ottmar <cara.ottmar@gmail.com>
2018-10-15 16:47:50 -0700

    added reverse

--- src/__tests__/lib/linkedlist.test.js
+++ src/__tests__/lib/linkedlist.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let LinkedList = require('../../lib/linkedlist');
+let reverse = require('../../lib/linkedlist');
 
 describe('Linked List Constructor', () => {
   it('constructor should be null', () => {
@@ -15,9 +16,9 @@ describe('Linked List Constructor', () => {
       list.append(2);
       list.append(3);
 
-      expect(list.head.value).toBe(1);
+      expect(list.value).toBe(3);
       expect(list.head.next.value).toBe(2);
-      expect(list.head.next.next.value).toBe(3);
+      expect(list.head.next.next.value).toBe(1);
     });
   });
 
@@ -31,6 +32,26 @@ describe('Linked List Constructor', () => {
       expect(list.head.value).toBe('Cara');
       expect(list.head.next.value).toBe('Birdie');
       expect(list.head.next.next.value).toBe('Billie Jean');
+    });
+  });
+
+  describe('A reversed Singly Linked List', () => {
+    it('Should return with the head at the end, and the tail at the head', () => {
+      let list = new LinkedList();
+
+      list.append(1);
+      list.append(2);
+      list.append(3);
+      list.append(4);
+
+      expect(list.head.value).toEqual(4);
+      expect(list.next.value).toEqual(3);
+      expect(list.next.next.value).toEqual(2);
+      expect(list.next.next.next.value).toEqual(1);
+    });
+    test('should err out if list is empty', () => {
+      const list = new LinkedList();
+      expect(() => reverse(list)).toThrow('missing input');
     });
   });
 });
--- src/lib/linkedlist.js
+++ src/lib/linkedlist.js
@@ -32,6 +32,35 @@ class LinkedList {
     node.next = this.head;
     this.head = node;
   }
+
+  reverse(list) {
+    if (!list) throw new Error ('missing input');
+    if (!list.head) throw new Error ('list must not be empty');
+    let prevNode = null;
+    let currNode = list.head;
+    let nextNode = null;
+
+    while(currNode) {
+      nextNode = currNode.next;
+      currNode.next = prevNode;
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+    return prevNode;
+  }
 }
 
 module.exports = LinkedList;
+
+  //   let curr = this.head;
+  //   let next = null;
+  //   let prev = null;
+
+  //   while(curr) {
+  //     next = curr.next;
+  //     curr.next = prev;
+  //     prev = curr;
+  //     curr = next;
+  //   }
+  //   this.head = prev;
+  // }
